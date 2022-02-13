890208ca9a425d008230c250ea5ce514c0aaec6b
FedeCana00 <federico.canali00@gmail.com>
2022-02-14 00:27:07 +0100

    Started to perform search by film name

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,6 +7,28 @@ import { useState } from "react";
 
 export default function Navbar() {
     const [showGeneres, setShowGeneres] = useState(false);
+    const [showSearchBar, setShowSearchBar] = useState(false);
+    const [textSearch, setTextSearch] = useState("");
+
+    function show(isGeneres){
+        if(isGeneres){
+            setShowGeneres(!showGeneres);
+            if(showSearchBar)
+                setShowSearchBar(false);
+        } else {
+            setShowSearchBar(!showSearchBar);
+            if(showGeneres)
+                setShowGeneres(false);
+        }
+    }
+
+    function textChanged(text){
+        setTextSearch(text);
+
+        // perform search if text is different from ""
+        // else redirect to home
+        
+    }
 
     return (
         <div className="navbar">
@@ -23,8 +45,11 @@ export default function Navbar() {
                             <li>
                                 <Link to="/">Home</Link>
                             </li>
-                            <li onClick={() => setShowGeneres(!showGeneres)}>
+                            <li onClick={() => show(true)}>
                                 <a>Generes</a>
+                            </li>
+                            <li onClick={() => show(false)}>
+                                <a>Search</a>
                             </li>
                         </ul>
                     </div>
@@ -33,12 +58,22 @@ export default function Navbar() {
                     {window.sessionStorage.getItem('email') === null ? <Disconnected /> : <Connected />}
                 </div>
             </div>
-            <div className={`generes ${showGeneres ? "active" : ""}`}>
+
+            {/* Box of genere */}
+            <div className={`box ${showGeneres ? "active" : ""}`}>
                 {generes.map((d) => (
                     <Link to={"/films/" + d.name}>
                         {d.name}
                     </Link>
                 ))}
+            </div>
+
+            {/* Box of search bar */}
+            <div className={`box ${showSearchBar ? "active" : ""}`}>
+                <input type="text" placeholder="Insert here the film name..." 
+                    onChange={(event) => {textChanged(event.target.value)}}/>
+                <img src="assets/search.png" alt="search" />
+                <img src="assets/close.png" alt="clear"/>
             </div>
 
             <Outlet />
