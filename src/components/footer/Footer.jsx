f29895d74fe6cb51d1a1b8a92ec367f9451b70ce
FedeCana00 <federico.canali00@gmail.com>
2022-02-19 18:03:07 +0100

    Footer created

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+export default function Footer() {
+
+    return (
+        <div className="footer" id="footer">
+            <img src="assets/icon.png" alt="" />
+            <div className="name">Federico Canali</div>
+            <div className="name">Andrea Adorni</div>
+        </div>
+    );
+}
